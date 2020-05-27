ab81de28c6a552630453b8819d45cb71ca7e94ae
Gotaeyoun <ohdin486@gmail.com>
2020-05-27 18:12:57 +0900

    완료

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+/***************** 이벤트 함수 ***********************/
+function fixShow(show){
+	if(show){
+		$(".header > .fix-wrap").css("display", "block");
+		setTimeout(function(){
+		$(".header > .fix-wrap").addClass("active");
+		$(".header > .fix-wrap > div").addClass("active");
+		},0);
+	}
+	else{
+		$(".header > .fix-wrap").removeClass("active");
+		$(".header > .fix-wrap").removeClass("active");
+		setTimeout(function(){
+			$(".header > .fix-wrap").css("display", "none");
+		},500);
+	}
+}
+
+
+
 
 
 /***************** 이벤트 콜백 ***********************/
@@ -10,18 +30,27 @@ function onNaviLeave(){
 
 function onBarClick(){
 	if($(this).hasClass("default")){
-		$(this).removeClass("default").addClass("active"); 
+		$(this).removeClass("default").addClass("active");
+		fixShow(true); 
 	}
 	else if($(this).hasClass("active")){
 		$(this).removeClass("active").addClass("default"); 
+		fixShow(false);
 	}
 	else{
 		$(this).addClass("active");
+		fixShow(true);
 	}
+	
 }
 
-
+function onNaviChildClick(){
+	$(this).next().stop().slideToggle(500);
+	$(this).children("i").toggleClass("active");
+}
 
 /***************** 이벤트 선언 ***********************/
 $(".header .navi-child").hover(onNaviHover,onNaviLeave);
 $(".header .navi-bars").click(onBarClick);
+$(".header .navi-child-mo").click(onNaviChildClick);
+
