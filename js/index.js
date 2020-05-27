9328b59532ac6e2e3a84e0f29f38c1be116389a5
Gotaeyoun <ohdin486@gmail.com>
2020-05-27 16:52:08 +0900

    navi css clear

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+
+/***************** 이벤트 콜백 ***********************/
 function onNaviHover(){
 	$(this).find(".subs").stop().fadeIn(500);
 }
@@ -5,4 +8,20 @@ function onNaviLeave(){
 	$(this).find(".subs").stop().fadeOut(500);
 }
 
-$(".navi-child").hover(onNaviHover,onNaviLeave);
+function onBarClick(){
+	if($(this).hasClass("default")){
+		$(this).removeClass("default").addClass("active"); 
+	}
+	else if($(this).hasClass("active")){
+		$(this).removeClass("active").addClass("default"); 
+	}
+	else{
+		$(this).addClass("active");
+	}
+}
+
+
+
+/***************** 이벤트 선언 ***********************/
+$(".header .navi-child").hover(onNaviHover,onNaviLeave);
+$(".header .navi-bars").click(onBarClick);
